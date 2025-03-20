0e2f78c1548e2aa49111cb6a976c8a9532a1313a
hjkim24 <hjkim4842@naver.com>
2025-03-20 22:19:55 +0900

    customOauth2userservice에 apple 로그인 추가

--- src/main/java/com/chihuahua/sobok/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/chihuahua/sobok/oauth/CustomOAuth2UserService.java
@@ -2,6 +2,7 @@ package com.chihuahua.sobok.oauth;
 
 import com.chihuahua.sobok.member.Member;
 import com.chihuahua.sobok.member.MemberService;
+import com.nimbusds.jose.JOSEException;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,6 +15,8 @@ import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
@@ -26,6 +29,7 @@ public class CustomOAuth2UserService extends OidcUserService {
     private final MemberService memberService;
     private final OauthAccountRepository oauthAccountRepository;
     private final KakaoIdTokenValidator kakaoIdTokenValidator;
+    private final ApplePublicKeyService applePublicKeyService;
 
 
     @Override
@@ -123,6 +127,47 @@ public class CustomOAuth2UserService extends OidcUserService {
                         return oauthAccountRepository.save(newAccount);
                     });
 
+        } else if("apple".equals(registrationId)) {
+            try {
+                // Apple 로그인 시 id_token 검증
+                String idToken = userRequest.getIdToken().getTokenValue();
+                Claims claims = (Claims) applePublicKeyService.verifyToken(idToken);
+
+                //1. Apple 로그인 정보 가져오기
+                String appleId = claims.get("sub", String.class);
+                String appleEmail = claims.get("email", String.class);
+                String appleName = claims.get("name", String.class);
+
+                // 2. member 테이블에 저장 (회원가입 이력이 없는 경우)
+                Member member = memberService.findByEmail(appleEmail)
+                        .map(existingMember -> {
+                            existingMember.setIsOauth(true);
+                            return memberService.saveOrUpdate(existingMember); // 이메일 정보가 있는 경우에는 oauth 정보만 업데이트
+                        })
+                        .orElseGet(() -> {
+                            Member newMember = new Member();
+                            newMember.setEmail(appleEmail);
+                            newMember.setName(appleName);
+                            newMember.setCreatedAt(LocalDateTime.now());
+                            newMember.setPoint(0);
+                            newMember.setUsername(appleId);
+                            newMember.setIsOauth(true);
+                            return memberService.saveOrUpdate(newMember);
+                        });
+
+                // 3. oauth_account 테이블에 저장
+                OauthAccount oauthAccount = oauthAccountRepository.findByOauthIdAndProvider(appleId, registrationId)
+                        .orElseGet(() -> {
+                            OauthAccount newAccount = new OauthAccount();
+                            newAccount.setOauthId(appleId);
+                            newAccount.setProvider(registrationId);
+                            newAccount.setCreatedAt(LocalDateTime.now());
+                            newAccount.setMember(member);
+                            return oauthAccountRepository.save(newAccount);
+                        });
+            } catch (ParseException | IOException | JOSEException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         OidcUser oidcUser = super.loadUser(userRequest);
